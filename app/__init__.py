d100d8ab2a044ff2f19b110125274df85ce36e7c
Alan Ng <alanq.ng@gmail.com>
2018-10-03 19:22:32 +0200

    Populate database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
+db = SQLAlchemy(app)
 
-from app import routes
+from app import routes, models
 
