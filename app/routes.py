b5173a9e8f22429f604e1fc2fc2eeaafa6a7a3a7
Alan Quin NG Martinez <AlanQ.NG@cz.abb.com>
2018-10-03 12:20:54 +0200

    Add routes

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,30 @@ from app import app
 
 
 @app.route('/')
-def index():
-    return 'Hello World'
+@app.route('/restaurants')
+def show_restaurants():
+    return 'This page will show all my restaurants'
+
+@app.route('/restaurant/new')
+def new_restaurant():
+    return 'This page will be for making a new restaurant'
+
+@app.route('/restaurant/restaurant_id/edit')
+def edit_restaurant():
+    return f'This page will be for editing restaurant {restaurant_id}'
+
+@app.route('/restaurant/restaurant_id/delete')
+def delete_restaurant():
+    return f'This page will be for deleting restaurant {restaurant_id}'
+
+@app.route('/restaurant/restaurant_id/menu/new')
+def new_menu_item():
+    return f'This page is for making a new item for restaurant {restaurant_id}'
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/edit')
+def edit_menu_item():
+    return f'This page will be for editing menu {menu_id}'
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/delete')
+def delete_menu_item():
+    return f'This page will be for deleting menu {menu_id}'
